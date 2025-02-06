5c2cfb60c56257fe4d838b12c8baa3db9cf42171
Tanvir Rahman Shuvo <shuvotanvir702@gmail.com>
2025-02-06 20:31:28 +0600

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ console.log(user);
 
 //method returns the system uptime in seconds
 console.log(`The system uptime is ${os.uptime()} seconds`);
+
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMemory: os.totalmem(),
+    freeMemory: os.freemem()
+}
+
+console.log(currentOS);

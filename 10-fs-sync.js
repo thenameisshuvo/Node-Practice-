60a331d817531ddf16677695dd4fd2d4d90c7bfa
Tanvir Rahman Shuvo <shuvotanvir702@gmail.com>
2025-02-11 18:34:26 +0600

    update

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,14 +1,24 @@
-const {readFileSync,writeFileSync} = require('fs');
+const { readFileSync, writeFileSync } = require("fs");
+console.log("start");
 
-const first = readFileSync('./contents/subfolder/first.txt', 'utf8');
-const second = readFileSync('./contents/subfolder/second.txt', 'utf8');
-console.log(first, second)
+const first = readFileSync("./contents/subfolder/first.txt", "utf8");
+const second = readFileSync("./contents/subfolder/second.txt", "utf8");
+console.log(first, second);
 
+writeFileSync("./contents/subfolder/first.txt", "Hello, World!");
+writeFileSync("./contents/subfolder/second.txt", "Hello, Bangladesh!");
 
-writeFileSync('./contents/subfolder/first.txt', 'Hello, World!');
-writeFileSync('./contents/subfolder/second.txt', 'Hello, Bangladesh!');
-
-writeFileSync('./contents/subfolder/result-sync.txt', `Here is the result: ${first}, ${second}`, {flag: 'a'});
+writeFileSync(
+  "./contents/subfolder/result-sync.txt",
+  `Here is the result: ${first}, ${second}`,
+  { flag: "a" }
+);
 // Here is the result: Hello, World!, Hello, Bangladesh!
 // flag: 'a' means append
 
+console.log("done with this task");
+console.log("starting the next one");
+// start
+// Hello, World! Hello, Bangladesh!
+// done with this task
+// starting the next one

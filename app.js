69e7bd2512188e5cf665855527e51623e119ee19
Tanvir Rahman Shuvo <shuvotanvir702@gmail.com>
2025-02-12 19:21:25 +0600

    update code

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-const http = require("http");
+const _ = require("lodash");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("Welcome to our home page");
-  }
-  if (req.url === "/about") {
-    res.end("Here is our short history");
-  }
-  res.end(`
-    <h1>Oops!</h1>
-    <p>We can't seem to find the page you are looking for</p>
-    <a href="/">back home</a>
-    `);
-});
+const items = [1, [2, [3, [4]]]];
 
-server.listen(8002);
-// console.log("Server is running on port 8002");
+const newItems = _.flattenDeep(items);
+console.log(newItems); // [1, 2, 3, 4]

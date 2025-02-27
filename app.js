b6edd4a7b091f78c90f2d4389cab80fd2ee48896
Tanvir Rahman Shuvo <shuvotanvir702@gmail.com>
2025-02-27 16:07:22 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-const _ = require("lodash");
+const { readFile, writeFile } = require("fs");
 
-const items = [1, [2, [3, [4]]]];
+console.log("start");
 
-const newItems = _.flattenDeep(items);
-console.log(newItems); // [1, 2, 3, 4]
+readFile("./contents/subfolder/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile("./contents/subfolder/second.txt", "utf8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile(
+      "./contents/subfolder/result-async.txt",
+      `Here is the result: ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log("done with this task");
+      }
+    );
+  });
+});
+
+console.log("starting next task");
